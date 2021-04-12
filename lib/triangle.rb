f5fa1a6c741b737239d136e4dd142b56595f07ec
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-12 17:47:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,10 +5,16 @@ class Triangle
   def initialize(a, b, c)
     @a = a
     @b = b
-    @c = c 
+    @c = c
   end
 
   def kind
-
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
   end
 end
